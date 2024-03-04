57c34afaa8f769fa3a82309c4a3e64ded730a7f4
maryam-jpg <maryam-jpg@users.noreply.github.com>
2024-03-03 18:25:11 -0600

    almost done, everything is set, just need to set up tabular display

--- a3regex.py
+++ a3regex.py
@@ -14,43 +14,51 @@ def main():
     regex = input("enter your regular expression: ")
     regex_new = str2regexp(regex)
     print(regex_new)
+    nfa_dfa_dict = dict()
     list_of_strings = input("enter your list of strings, separated by one space: ")
     list_of_strings = list_of_strings.split(" ")
-    regex_to_nfa(regex_new, list_of_strings)
-    regex_to_dfa(regex_new, list_of_strings)
+    regex_to_nfa(regex_new, list_of_strings, nfa_dfa_dict)
+    regex_to_dfa(regex_new, list_of_strings, nfa_dfa_dict)
+    print(nfa_dfa_dict)
 
 
-def regex_to_nfa(regex, list_of_strings):
+def regex_to_nfa(regex, list_of_strings, nfa_dfa_dict):
     # convert to nfa, record time to convert as well
     start_time = time.time()
     nfa = regex.nfaPosition()
     end_time = time.time()
+    print("---------------------------------------------------------------")
     print("Time to convert to NFA:", end_time - start_time, "seconds")
     # obtain the states in the nfa
-    states = len(nfa.States)
+    print("Number of states:", len(nfa.States))
+    print("---------------------------------------------------------------")
     for string in list_of_strings:
         start_time = time.time()
         nfa_acceptance = nfa.evalWordP(string)
         end_time = time.time()
+        nfa_dfa_dict[string] = [nfa_acceptance, end_time - start_time]
         print("NFA accepts", string, ":", nfa_acceptance, "(Time taken:", end_time - start_time, "seconds)")
+    print("---------------------------------------------------------------")
+    return nfa_dfa_dict
 
-    print(states)
 
-
-def regex_to_dfa(regex, list_of_strings):
+def regex_to_dfa(regex, list_of_strings, nfa_dfa_dict):
     # convert to dfa, record time to convert as well
     start_time = time.time()
     dfa = regex.nfaPosition().toDFA()
     end_time = time.time()
+    print("---------------------------------------------------------------")
     print("Time to convert to DFA:", end_time - start_time, "seconds")
     # obtain the states in dfa
-    states = len(dfa.States)
+    print("Number of states:", len(dfa.States))
+    print("---------------------------------------------------------------")
     for string in list_of_strings:
         start_time = time.time()
         dfa_acceptance = dfa.evalWordP(string)
         end_time = time.time()
+        nfa_dfa_dict[string] += [dfa_acceptance, end_time - start_time]
         print("DFA accepts", string, ":", dfa_acceptance, "(Time taken:", end_time - start_time, "seconds)")
-    print(states)
+    print("---------------------------------------------------------------")
 
 
 if __name__ == "__main__":
